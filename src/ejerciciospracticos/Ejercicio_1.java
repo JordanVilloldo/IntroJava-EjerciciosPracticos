b1923748c51bb7e71b2750aeaf8c31256ab1f96b
Jordan Villoldo <jordaanla1323@gmail.com>
2023-06-01 21:57:55 -0300

    Segunda carga, avance ejercicio 14,15,16,17. Y tercera, solo cambie el nombre del ejecicio 1

--- src/ejerciciospracticos/Ejercicio_1.java
+++ src/ejerciciospracticos/Ejercicio_1.java
@@ -15,7 +15,7 @@ import javax.swing.JOptionPane;
  * Escribir un programa que pida dos números enteros por teclado y calcule la
  * suma de los dos. El programa deberá después mostrar el resultado de la suma
  */
-public class Ejercicio1 {
+public class Ejercicio_1 {
 
     public static void main(String[] args) {
         int num1 = Integer.parseInt(JOptionPane.showInputDialog("Ingresa el primer numero"));
